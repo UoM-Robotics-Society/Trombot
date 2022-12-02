77e2e651b56f36ed665754ef41b0ae61e819ff8d
DefinitelyBalanced <72870238+DefinitelyBalanced@users.noreply.github.com>
2022-12-02 19:32:52 +0000

    It worked!!! In my defence the code was perfect, it was the components fault.

--- Code/Trombot/src/main.cpp
+++ Code/Trombot/src/main.cpp
@@ -1,32 +1,40 @@
 #include <Arduino.h>
 #include "Sterpper.h"
-#include "multisteppertb62269.h"
 
-#define Controller_dir 7
-#define Controller_pul 6
+#define Controller_dir 12
+#define Controller_pul 11
+#define Controller_MS1 2
+#define Controller_MS2 3
 
 Stepper Slide(Controller_pul, Controller_dir);
 
 void setup() {
-  // put your setup code here, to run once:
+
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(2000);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.begin(9600);
+
   pinMode(Controller_pul, OUTPUT);
   pinMode(Controller_dir, OUTPUT);
+  pinMode(Controller_MS1, OUTPUT);
+  pinMode(Controller_MS2, OUTPUT);
+
   digitalWrite(Controller_dir, HIGH);
+  digitalWrite(Controller_MS2, LOW); //Change this for microstepping https://www.schmalzhaus.com/EasyDriver/
+  digitalWrite(Controller_MS1, LOW); //see above
+
 
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("pulsing!");
+  Serial.println("pulsing");
   digitalWrite(Controller_pul, HIGH);
-  delayMicroseconds(5);
+  delayMicroseconds(100);
   digitalWrite(Controller_pul, LOW);
-  delayMicroseconds(5);
-  delay(200);
+  delayMicroseconds(100);
+  delay(5);
 }
